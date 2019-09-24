d9f0476ffaaaa542d39057cd5606ab209f520a22
fiqriakbar9 <42399322+fiqriakbar9@users.noreply.github.com>
2019-09-24 20:56:44 +0700

    Add files via upload

---
+++ src/kambingstatic/Mamalia.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package kambingstatic;
+
+/**
+ *
+ * Nama : Fiqri Akbar Pratama
+ * NIM  : 10118020  
+ * Kelas: IF-1 
+ * Deskripsi Program : Program ini berisi program untuk sekedar 
+ * mencoba menggunakan implementasi 2 class
+ */
+public class Mamalia {
+    public static int jumlahKambing;
+}
